dfb6802f276147e86b0b96dbfa5fe39dab572291
Romeo <romeogescolano@gmail.com>
2022-11-20 13:23:48 +0800

    add: hide and show of button if the page cannot be change

--- script.js
+++ script.js
@@ -14,14 +14,23 @@
 // }
 var page = 0;
 
+var btnl = document.getElementById('btnl');
+var btnr = document.getElementById('btnr')
+
+ 
 function changeQuestion(n) {
     var i;
     var qdiv = document.getElementsByClassName('questionaireDiv');
     if((page == 0 && n==-1)||(page==9 && n==+1)){
         n = 0;
-        page = page;
+
+        
+        //page==0 ? btnl.style.display = 'none' : btnl.style.display = 'block';
+        //page==9 ? btnr.style.display = 'none' : btnr.style.display = 'block';
     } else {
         page = page + n; 
+        page==0 ? btnl.style.display = 'none' : btnl.style.display = 'block';
+        page==9 ? btnr.style.display = 'none' : btnr.style.display = 'block';
         for (i=0;i<qdiv.length;i++){
             page==i ? qdiv[i].style.display= 'block' : qdiv[i].style.display='none';  
         }
